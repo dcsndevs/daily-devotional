1a26981da54a73caca8e3c6479e9438af17da36d
Dickson <olejemedickson@gmail.com>
2024-02-15 21:06:32 +0000

    Wire up archive, detailview and currentdevotional

---
+++ devotional/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import current_date_devotional
+
+urlpatterns = [
+    path('archive', views.PostList.as_view(), name='archive'),
+    # path('', views.post_list, name='post_list'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', current_date_devotional, name='current_date_devotional'),  # Add this line for the PostDetailView
+]
--- devotional/views.py
+++ devotional/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.views import generic
+from django.views.generic import DetailView
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
+# def post_list(request):
+#     posts = Post.objects.filter(status=1)
+#     return render(request, 'devotional/post_list.html', {'posts': posts})
+
+class PostList(generic.ListView):
+    current_date = timezone.now().date()
+    # queryset = Post.objects.filter(active_date__lt=current_date)
+    queryset = Post.objects.filter()
+    template_name = "devotional/archive.html"
+
+
+def post_detail(request, slug):
+    current_date = timezone.now().date()
+    post = get_object_or_404(Post, slug=slug, status=1)
+    return render(request, 'devotional/post_detail.html', {'post': post})
+
+
+def current_date_devotional(request):
+    # Retrieve the current date
+    current_date = timezone.now().date()
+    # Retrieve the post with the current date
+    post = Post.objects.filter(active_date=current_date, status=1).first()
+    return render(request, 'devotional/index.html', {'post': post})
