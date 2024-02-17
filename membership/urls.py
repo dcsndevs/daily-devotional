a65b162b50af9358671f72a085779603edeebd3e
Dickson <olejemedickson@gmail.com>
2024-02-17 21:31:38 +0000

    Add edit and delete frames

--- membership/urls.py
+++ membership/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 
 urlpatterns = [
-
     path('create/', views.new_membership, name='new_membership'),
     path('', views.display_membership, name='membership'),
-    #path('<slug:owner>/', views.new_membership, name='new_membership'),
-    #path('comment/<int:comment_id>/like/', views.like_comment, name='like_comment'),
-    #path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
-    #path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+    path('edit/', views.edit_membership, name='edit_membership'),
+    path('<slug:slug>/edit_profile/<int:owner_id>', views.edit_profile, name='edit_profile'),
+    path('<slug:slug>/delete_profile/<int:owner_id>', views.delete_profile, name='delete_profile'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.utils import timezone
 from django.views import generic, View
-from django.http import HttpResponse, HttpRequest, request
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.utils import timezone
 from django.contrib import messages
 from django.http import JsonResponse
@@ -36,7 +36,45 @@ def new_membership(request):
 
     creation_form = MembershipForm(user_id=request.user.id)
     profile = Membership.objects.filter(status=1).first()
-    return render(request, 'membership/create.html', { 'profile': profile,
-                                                        'creation_form': creation_form,
-                                                    }
-                  )
+    return render(request, 'membership/create.html', { 'profile': profile, 'creation_form': creation_form,})
+    
+def edit_membership(request):
+    profile = Membership.objects.filter(status=1).first()
+    edit_form = MembershipForm(user_id=request.user.id)
+    return render(request, 'membership/edit.html', { 'profile': profile, 'edit_form': edit_form,})
+
+
+def edit_profile(request, id, owner_id):
+
+    if request.method == "POST":
+        queryset = Membership.objects.filter(owner_id=request.user.id)
+        post = get_object_or_404(queryset, id=id)
+        profile = get_object_or_404(Membership, pk=owner_id)
+        membership_form = MembershipForm(data=request.POST, instance=profile)
+
+        if membership_form.is_valid() and profile.user == request.user:
+            profile = membership_form.save(commit=False)
+            profile.post = post
+            profile.save()
+            messages.add_message(request, messages.SUCCESS, 'Profile Update is successfully!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error: Could not update Profile!')
+
+    return HttpResponseRedirect(reverse('edit_membership', args=[id]))
+
+
+def delete_profile(request, id, owner_id):
+    """
+    view to delete profile
+    """
+    queryset = Membership.objects.filter(status=1)
+    post = get_object_or_404(queryset, id=id)
+    profile = get_object_or_404(Membership, pk=owner_id)
+
+    if profile.user == request.user:
+        profile.delete()
+        messages.add_message(request, messages.SUCCESS, 'Profile deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own profile!')
+
+    return HttpResponseRedirect(reverse('edit_membership', args=[id]))
