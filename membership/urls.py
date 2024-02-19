117bb085a65217885f2af6c53f8e4287cbb8a570
Dickson <olejemedickson@gmail.com>
2024-02-19 00:01:40 +0000

    Fix CRUD logic errors

--- membership/urls.py
+++ membership/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.display_membership_profile, name='membership'),
+    path('', views.display_membership_profile, name='view_membership_profile'),
     path('create/', views.new_membership_profile, name='new_membership_profile'),
-    path('edit/<int:pk>/', views.update_membership_profile.as_view(), name='update_membership_profile'),
+    path('delete/success/', views.DeleteSuccessView, name='membership_delete_success'),
     path('delete/<int:pk>/', views.delete_membership_profile.as_view(), name='delete_membership_profile'),
+    path('edit/<int:pk>/', views.update_membership_profile.as_view(), name='update_membership_profile'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.utils import timezone
 from django.views.generic import UpdateView, DeleteView
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
@@ -13,79 +13,35 @@ from .forms import MembershipForm
 # Create your views here.
 def display_membership_profile(request):
     profile = Membership.objects.filter(owner=request.user).first()
-    if profile:
-        return render(request, 'membership/index.html', { 'profile': profile,})
-        
-        
-    else:
-        # if request.method == 'POST':
-        #     creation_form = MembershipForm(data=request.POST)
-        #     if creation_form.is_valid():
-        #         creation = creation_form.save(commit=False)
-        #         creation.author = request.user
-        #         creation.save()
-        
-        return render(request, 'membership/create.html', {'creation_form': MembershipForm(user_id=request.user.id),})
+    print(profile)
+    return render(request, 'membership/index.html', { 'profile': profile,})
     
     
 def new_membership_profile(request):
     if request.method == "POST":
-        creation_form = MembershipForm(request.POST, user_id=request.user.id)
+        creation_form = MembershipForm(request.POST)
         if creation_form.is_valid():
-            creation_form.save(user_id=request.user.id)
-            messages.add_message(request, messages.SUCCESS, "You are now a new member!")
+            # Save the form without committing to obtain an instance
+            membership = creation_form.save(commit=False)
+            # Set the owner ID to the current user ID
+            membership.owner_id = request.user.id
+            # Save the instance to the database
+            membership.save()
+            messages.success(request, "You are now a new member!")
+            return redirect('view_membership_profile')  # Redirect to the profile page after successful form submission
 
-    creation_form = MembershipForm(user_id=request.user.id)
-    profile = Membership.objects.filter(status=1).first()
-    return render(request, 'membership/create.html', { 'profile': profile, 'creation_form': creation_form,})
+    else:  # If request method is not POST
+        creation_form = MembershipForm()  # Create an empty form
+
+    profile = Membership.objects.filter(owner=request.user)
+    return render(request, 'membership/create.html', { 'profile': profile, 'creation_form': creation_form})
     
-# def edit_membership(request):
-#     profile = Membership.objects.filter(wner_id=request.user.id).first()
-#     edit_form = MembershipForm(user_id=request.user.id)
-#     return render(request, 'membership/edit.html', { 'profile': profile, 'edit_form': edit_form,})
-
-
-# def edit_profile(request, id, owner_id):
-
-#     if request.method == "POST":
-#         queryset = Membership.objects.filter(owner_id=request.user.id)
-#         post = get_object_or_404(queryset, id=id)
-#         profile = get_object_or_404(Membership, pk=owner_id)
-#         membership_form = MembershipForm(data=request.POST, instance=profile)
-
-#         if membership_form.is_valid() and profile.user == request.user:
-#             profile = membership_form.save(commit=False)
-#             profile.post = post
-#             profile.save()
-#             messages.add_message(request, messages.SUCCESS, 'Profile Update is successfully!')
-#         else:
-#             messages.add_message(request, messages.ERROR, 'Error: Could not update Profile!')
-
-#     return HttpResponseRedirect(reverse('edit_membership', args=[id]))
-
-
-# def delete_profile(request, id, owner_id):
-#     """
-#     view to delete profile
-#     """
-#     queryset = Membership.objects.filter(wner_id=request.user.id)
-#     post = get_object_or_404(queryset, id=id)
-#     profile = get_object_or_404(Membership, pk=owner_id)
-
-#     if profile.user == request.user:
-#         profile.delete()
-#         messages.add_message(request, messages.SUCCESS, 'Profile deleted!')
-#     else:
-#         messages.add_message(request, messages.ERROR, 'You can only delete your own profile!')
-
-#     return HttpResponseRedirect(reverse('membership', args=[id]))
-
 
 class update_membership_profile(UpdateView):
     model = Membership
     form_class = MembershipForm
     template_name = 'membership/update.html'
-    success_url = reverse_lazy('membership')
+    success_url = reverse_lazy('view_membership_profile')
     
     def form_valid(self, form):
         messages.success(self.request, 'Your Member profile has been successfully updated!')
@@ -95,9 +51,11 @@ class update_membership_profile(UpdateView):
 class delete_membership_profile(DeleteView):
     model = Membership
     template_name = 'membership/delete_member.html'
-    success_url = reverse_lazy('membership')
+    success_url = reverse_lazy('membership_delete_success')
     
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'Your Member profile has been successfully deleted!')
-        return super().delete(request, *args, **kwargs)
+        response = super().delete(request, *args, **kwargs)
+        return response
  
+def DeleteSuccessView(request):
+    return render(request, 'membership/delete_membership_profile_success.html')
