9a3f5949caa84942031ff315594ff6b3b06a1ba1
Dickson <olejemedickson@gmail.com>
2024-02-21 03:28:01 +0000

    Add all events html pages

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schedule', views.PostList.as_view(), name='programe_list'),
-    path('<slug:slug>/', views.post_detail, name='programme_detail'),
     path('', views.current_Programme, name='current_programmes'), 
+    path('schedule/', views.PostList.as_view(), name='programe_list'),
+    path('<slug:slug>/', views.post_detail, name='programme_detail'),
+    
 ]
--- events/views.py
+++ events/views.py
@@ -10,13 +10,14 @@ from .forms import GuestForm, MemberGuestForm
 
 
 class PostList(generic.ListView):
-    queryset = Programmes.objects.filter(status=1)
+    queryset = Programmes.objects.filter(slot=50, status=0)
     template_name = "events/event_list.html"
+    paginate_by = 6    
 
 
 def post_detail(request, slug):
     # post2 = Programmes.objects.all().filter(status=0).first()
-    post = get_object_or_404(Programmes, slug=slug, status=1)
+    post = get_object_or_404(Programmes, slug=slug, status=0)
     
     if request.method == "POST":
         guest_form = GuestForm(data=request.POST)
@@ -47,17 +48,16 @@ def post_detail(request, slug):
         "events/event_detail.html",
         {
             "post": post,
-            "guest_form": guest_registration_form,
+            # "guest_form": guest_registration_form,
             # "member_guest_form": member_guest_form
         },
     )
 
 
 def current_Programme(request):
-    current_date = timezone.now().date()
 
     # post = Programmes.objects.all().filter(status=0).first()
-    post = Programmes.objects.filter(active_date=current_date, status=1).first()
+    post = Programmes.objects.filter(status=0).order_by('?').first()
     if request.method == "POST":
         guest_form = GuestForm(data=request.POST)
         if guest_form.is_valid():
@@ -88,7 +88,7 @@ def current_Programme(request):
         "events/index.html",
         {
             "post": post,
-            "guest_form": guest_form,
+            #"guest_form": guest_form,
             # "member_guest_form": member_guest_form
         },
     )
