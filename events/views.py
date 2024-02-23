8116e5c5aab9384fffd6668ad34e6c0e7769a61e
Dickson <olejemedickson@gmail.com>
2024-02-23 19:40:31 +0000

    Configure events indexlogic

--- events/views.py
+++ events/views.py
@@ -56,29 +56,48 @@ def post_detail(request, slug):
     return render(request, 'events/event_detail.html', {'post': post, 'form': form, 'remaining_slot': remaining_slot})
     
 
-
 def current_Programme(request):
-    #this view should inherit from post_detail; the difference is the filter option that specifies which to show
-    # post = Programmes.objects.all().filter(status=0).first()
-    post = Programmes.objects.filter(status=0).order_by('?').first()
     
+    post = Programmes.objects.filter(status=1).order_by('?').first()
+    print(post.slot)
+    slot = post.slot
+    used_slot = Attendee.objects.filter(event=post.id).count()
+    email_per_event = Attendee.objects.filter(event=post.id)
+    remaining_slot = slot - used_slot
     
-    return render(
-        request,
-        "events/index.html",
-        {
-            'post': post,
-            #'form': form,
-        },
-    )
-
-
-def event_registration(request):
-    if request.method == 'POST':
+    if remaining_slot == 0:
+        
+        return render(request, 'events/index.html', {'post': post, 'remaining_slot': remaining_slot})
+    
+    elif request.method == "POST":
+        
         form = EventRegistrationForm(request.POST)
+        email = request.POST.get('email')
         if form.is_valid():
-            form.save()
-            return redirect('registration_success')
+            #to double check just incase a user has access to the form before slot finished
+            if remaining_slot == 0:
+                messages.warning(
+                    request,
+                    'Sorry this programme is fully booked!'
+                )
+                return render(request, 'events/index.html', {'post': post})
+            elif email not in email_per_event:
+                messages.warning(
+                    request,
+                    'Sorry this email has already been registered for this event!'
+                )
+                return render(request, 'events/index.html', {'post': post, 'form': form, 'remaining_slot': remaining_slot})
+                
+            else:
+                post_id = post.id
+                form.instance.event_id = post_id
+                form.save()
+                messages.success(
+                    request,
+                    'Registration submitted successfully!'
+                )
+                return redirect(reverse('home'))
     else:
         form = EventRegistrationForm(user=request.user)
-    return render(request, 'event_registration.html', {'form': form})
+    return render(request, 'events/index.html', {'post': post, 'form': form, 'remaining_slot': remaining_slot})
+ 
