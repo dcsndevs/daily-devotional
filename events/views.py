3aa63cf77dfff5446b1704aed111be317675a8a4
Dickson <olejemedickson@gmail.com>
2024-02-23 01:42:09 +0000

    Add programme slot logic

--- events/views.py
+++ events/views.py
@@ -2,60 +2,64 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Programmes
+from .models import Programmes, Attendee
 from .forms import GuestForm, EventRegistrationForm
 
 
 class PostList(generic.ListView):
-    queryset = Programmes.objects.filter(slot=50, status=1)
+    queryset = Programmes.objects.filter(status=1)
     template_name = "events/event_list.html"
     paginate_by = 6    
 
 
 def post_detail(request, slug):
     post = get_object_or_404(Programmes, slug=slug, status=1)
+    slot = post.slot
+    used_slot = Attendee.objects.filter(event=post.id).count()
+    print(used_slot)
+    remaining_slot = slot - used_slot
     
-    if request.method == "POST":
+    if remaining_slot == 0:
+        
+        return render(request, 'events/event_detail.html', {'post': post, 'remaining_slot': remaining_slot})
+    
+    elif request.method == "POST":
+        
         form = EventRegistrationForm(request.POST)
         if form.is_valid():
-            post_id = post.id
-            form.instance.event_id = post_id
-            form.instance.author = request.user
-            form.save()
-            messages.success(
-                request,
-                'Registration submitted successfully!'
-            )
-            return redirect(reverse('home'))
+            if remaining_slot == 0:
+                messages.success(
+                    request,
+                    'Sorry this programme is fully booked!'
+                )
+                return render(request, 'events/event_detail.html', {'post': post})
+            else:
+                post_id = post.id
+                form.instance.event_id = post_id
+                form.save()
+                messages.success(
+                    request,
+                    'Registration submitted successfully!'
+                )
+                return redirect(reverse('home'))
     else:
         form = EventRegistrationForm()
-    return render(request, 'events/event_detail.html', {"post": post, 'form': form})
+    return render(request, 'events/event_detail.html', {'post': post, 'form': form, 'remaining_slot': remaining_slot})
     
 
 
 def current_Programme(request):
-
+    #this view should inherit from post_detail; the difference is the filter option that specifies which to show
     # post = Programmes.objects.all().filter(status=0).first()
     post = Programmes.objects.filter(status=0).order_by('?').first()
-    if request.method == "POST":
-        guest_form = GuestForm(data=request.POST)
-        if guest_form.is_valid():
-            guest = guest_form.save(commit=False)
-            guest.author = request.user
-            guest.post = post
-            guest.save()
-            messages.add_message(
-            request, messages.SUCCESS,
-            'Resgistration submitted successfully!'
-        )
-                
+    
     
     return render(
         request,
         "events/index.html",
         {
-            "post": post,
-            "guest_form": guest_form,
+            'post': post,
+            #'form': form,
         },
     )
 
