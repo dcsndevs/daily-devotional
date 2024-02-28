b90e30e14e7c2fb4a99808152d212d51014022e5
Dickson <olejemedickson@gmail.com>
2024-02-28 15:32:44 +0000

    Fix like and comment color logic

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// Get all elements with the class "commentfocus"
+var linkElements = document.querySelectorAll(".commentfocus");
+
+// Add event listener to each link element
+linkElements.forEach(function(linkElement) {
+    linkElement.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default behavior of the link
+        // Find the textarea input with the id "id_body"
+        var commentInput = document.getElementById("id_body");
+        commentInput.focus(); // Focus on the textarea input
+    });
+});
+
