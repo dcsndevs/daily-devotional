a2876bb9671c52711e80130d76dfb9577ae8771b
Dickson <olejemedickson@gmail.com>
2024-03-04 00:15:16 +0000

    format membership views

--- membership/views.py
+++ membership/views.py
@@ -11,6 +11,7 @@ from django.urls import reverse_lazy
 from .models import Profile
 from .forms import ProfileForm
 
+
 class PostList(generic.ListView):
     """
     View for displaying a member profile details.
@@ -29,7 +30,8 @@ class PostList(generic.ListView):
     """
     queryset = Profile.objects.filter(status=1)
     template_name = "membership/community.html"
-    paginate_by = 12    
+    paginate_by = 12
+
 
 @login_required
 def display_membership_profile(request):
@@ -45,9 +47,9 @@ def display_membership_profile(request):
     :template:`membership/index.html`
     """
     profile = Profile.objects.filter(owner=request.user).first()
-    return render(request, 'membership/index.html', { 'profile': profile,})
-    
-    
+    return render(request, 'membership/index.html', {'profile': profile, })
+
+
 @login_required
 def new_membership_profile(request):
     """
@@ -66,7 +68,7 @@ def new_membership_profile(request):
         creation_form = ProfileForm(request.POST, request.FILES)
         if creation_form.is_valid():
             membership = creation_form.save(commit=False)
-            membership.owner = request.user  # Set the owner to the current user
+            membership.owner = request.user
 
             # Check if a picture was uploaded
             if 'picture' in request.FILES:
@@ -80,17 +82,20 @@ def new_membership_profile(request):
             # Save the membership and related user
             with transaction.atomic():
                 membership.save()
+                anchor = request.user
                 # Also update the User record in Auth
-                request.user.first_name = creation_form.cleaned_data['first_name']
-                request.user.last_name = creation_form.cleaned_data['last_name']
-                request.user.email = creation_form.cleaned_data['email']
-                request.user.save()
+                anchor.first_name = creation_form.cleaned_data['first_name']
+                anchor.last_name = creation_form.cleaned_data['last_name']
+                anchor.email = creation_form.cleaned_data['email']
+                anchor.save()
 
             messages.success(request, "You are now a new member!")
             return redirect('view_membership_profile')
     else:
         creation_form = ProfileForm(user=request.user)
-    return render(request, 'membership/create.html', {'creation_form': creation_form})
+    return render(request, 'membership/create.html',
+                  {'creation_form': creation_form})
+
 
 def post_detail(request, owner):
     """
@@ -105,7 +110,7 @@ def post_detail(request, owner):
 
     :template:`membership/slected-member-profile-detail.html`
     """
-    
+
     profile = get_object_or_404(Profile, owner=owner, status=1)
 
     return render(
@@ -115,7 +120,6 @@ def post_detail(request, owner):
             "profile": profile,
         },
     )
-
 
 
 class update_membership_profile(UpdateView):
@@ -129,14 +133,15 @@ class update_membership_profile(UpdateView):
 
     :template:`membership/update.html`
     """
-    
+
     model = Profile
     form_class = ProfileForm
     template_name = 'membership/update.html'
     success_url = reverse_lazy('view_membership_profile')
-    
+
     def form_valid(self, form):
-        messages.success(self.request, 'Your Member profile has been successfully updated!')
+        messages.success(self.request,
+                         'Your Member profile has been successfully updated!')
         return super().form_valid(form)
 
 
@@ -152,11 +157,12 @@ class delete_membership_profile(DeleteView):
     model = Profile
     template_name = 'membership/delete_member.html'
     success_url = reverse_lazy('membership_delete_success')
-    
+
     def delete(self, request, *args, **kwargs):
         response = super().delete(request, *args, **kwargs)
         return response
- 
+
+
 def DeleteSuccessView(request):
     """
     View for confirming deleted profile details.
@@ -168,15 +174,17 @@ def DeleteSuccessView(request):
     """
     return render(request, 'membership/delete_membership_profile_success.html')
 
+
 class PasswordsChangeView(PasswordChangeView):
     """
     View for changin profile member passord.
     **Context**
     """
-    
+
     form_class = PasswordChangeForm
     success_url = reverse_lazy('view_membership_profile')
-    
+
     def form_valid(self, form):
-        messages.success(self.request, 'Your Password has been successfully updated!')
+        messages.success(self.request,
+                         'Your Password has been successfully updated!')
         return super().form_valid(form)
